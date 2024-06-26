359c560d5998bce0689c23f1c977bcaa1207fc97
DenSad81 <tagil81@mail.ru>
2024-06-26 08:33:51 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence = "Раз два три четыре пять";
+            string[] arrayOfWords = sentence.Split(' ');
+
+            Console.WriteLine($"Предложение: {sentence}");
+            Console.WriteLine();
+
+            for (int i = 0; i < arrayOfWords.Length; i++)
+                Console.WriteLine($"Слово: {arrayOfWords[i]}");
+
+            Console.WriteLine();
+        }
+    }
